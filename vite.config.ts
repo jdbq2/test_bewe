1ce8be3ea0801a33affc73e2c2384f1a04bb12cc
Juan Diego Bohorquez Quiroga <jdbq2@hotmail.com>
2023-06-03 20:27:41 -0500

    Bases de las apis configuradas

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  /*
   server: {
+    /*
     proxy: {
       "/api": {
-        target: "http://tecnical.bewe.co",
+        target: "https://tecnical.bewe.co",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-*/
+  */
   plugins: [react()],
 });
